85abc4593772ef6658a3497bb4402ef4a66b5a8e
eduardo.gashu <eduardo.gashu@creditas.com.br>
2020-06-23 11:29:06 -0300

    add processing time report at the end of the execution

--- rating_calculator/src/app/main.js
+++ rating_calculator/src/app/main.js
@@ -1,15 +1,38 @@
 const preprocess = require("./useCases/preprocess");
 const sharpRate = require("./useCases/sharpRate");
+const gsRate = require("./useCases/gsRate");
+const now = require("performance-now")
 const prompt = require("prompt")
 
 prompt.start();
 
-prompt.get(['helmetName'], function (err, result) {
+console.log(`Hello! This is the Motorcycle Helmet's Safety Rating Calculator`);
+console.log(`Please enter the helmet that you want to rate`);
+console.log(`And choose the calculation method: sharp, gs or all`);
+
+prompt.get(['helmetName', 'ratingMethod'], function (err, result) {
+  let t1 = now();
   if (err) { return onErr(err); }
-  console.log('"Please enter the helmet name":');
   console.log('  Helmet Name: ' + result.helmetName);
+  console.log('  Rating Method: ' + result.ratingMethod);
   preprocess(result.helmetName);
-  sharpRate(result.helmetName);
+  switch(result.ratingMethod.toUpperCase()){
+    case "SHARP":
+      sharpRate(result.helmetName);
+      break;
+    case "GS":
+      gsRate(result.helmetName);
+      break;
+    case "ALL":
+      sharpRate(result.helmetName);
+      gsRate(result.helmetName);
+      break;
+    default:
+      console.log(`Rating method ${result.ratingMethod} not found.`);
+      return;
+  }
+  let t2 = now();
+  console.log("Processing time " + (t2 - t1) + " milliseconds.");
   console.log(`The outputs can be found in rating_calculator/data/${result.helmetName}`);
 });
 
